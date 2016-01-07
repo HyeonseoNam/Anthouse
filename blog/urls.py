5542c4f046ee6d32aa5b0c9cb363a440f432a8ad
blue1028 <blue1028@unist.ac.kr>
2016-01-07 21:56:25 +0900

    로그인 제한, 글쓰기 수정 삭제  등 추가

--- blog/urls.py
+++ blog/urls.py
@@ -1,19 +1,25 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
 
-urlpatterns = [
-    url(r'^$', 'blog.views.index', name='index'),
-    url(r'^(?P<pk>\d+)/$', 'blog.views.post_detail'),
-    url(r'^(?P<pk>\d+)/comments/new/$', 'blog.views.comment_new'), #새 댓글생성
-    url(r'^(?P<post_pk>\d+)/comments/(?P<pk>\d+)/edit/$', 'blog.views.comment_edit'), #댓글 수정
+urlpatterns = patterns('blog.views',
+    url(r'^$', 'index', name='index'),
+    url(r'^(?P<pk>\d+)/$', 'post_detail', name='post_detail'),
+    url(r'^new/$', 'post_create', name='post_create'),
+    url(r'^(?P<pk>\d+)/edit/$', 'post_update', name='post_update'),
+    url(r'^(?P<pk>\d+)/delete/$', 'post_delete', name='post_delete'),
+
+
+    url(r'^(?P<pk>\d+)/comments/new/$', 'comment_new', name='comment_new'), #새 댓글생성
+    url(r'^(?P<post_pk>\d+)/comments/(?P<pk>\d+)/edit/$', 'comment_edit', name='comment_edit'), #댓글 수정
+    url(r'^(?P<post_pk>\d+)/comments/(?P<pk>\d+)/delete/$', 'comment_delete', name='comment_delete'), #댓글 삭제
 
 
 
-    url(r'^new/$', 'blog.views.post_new'),
-]
+
+    )
 
 
 # urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
