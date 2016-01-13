099acc8cfba1239d48bdfe0bc8855d7e5d40c8e6
blue1028 <blue1028@unist.ac.kr>
2016-01-14 02:00:44 +0900

    사이드 네비게이션 수정 / Chart 추가 new

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from .models import Post, Comment
 from .forms import CommentForm, PostForm, TimelineForm
 from django.contrib.auth.decorators import login_required
-
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def test(request):
     post_list = Post.objects.all().order_by('-created_at')
@@ -26,75 +27,146 @@ def chart(request):
     })
 
 
-def index(request):
-    post_list = Post.objects.all()
-    return render(request, 'blog/index.html', {
-        'post_list': post_list,
-    })
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+index = PostListView.as_view()
+
+#
+#
+# def index(request):
+#     post_list = Post.objects.all()
+#     return render(request, 'blog/index.html', {
+#         'post_list': post_list,
+#     })
 
 def post_detail(request, pk):
     post= Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {
         'post':post,
     })
-@login_required
-def post_create(request):
 
-    #request.GET
-    #request.POST
-    #request.FILES
+# @login_required
+# def post_create(request):
+#
+#     #request.GET
+#     #request.POST
+#     #request.FILES
+#
+#     #request.user =>
+#     # django.contrib.auth.models.AnonymousUser 로그인 안됐을때 자동 할당
+#     # django.contrib.auth.models.User  로그인 됐을때 자동 할당
+#     #
+#
+#
+#     if request.method == 'POST':
+#         form = PostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.save()
+#             return redirect('blog:post_detail', post.pk)
+#     else:
+#         form = PostForm()
+#     return render(request, 'blog/post_forms.html',{
+#         'form':form,
+#     })
 
-    #request.user =>
-    # django.contrib.auth.models.AnonymousUser 로그인 안됐을때 자동 할당
-    # django.contrib.auth.models.User  로그인 됐을때 자동 할당
-    #
 
 
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('blog:post_detail', post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_forms.html',{
-        'form':form,
-    })
+class PostCreateView(CreateView):
+    model=Post
+    form_class = PostForm
+    template_name = 'blog/post_forms.html'
 
-@login_required
-def post_update(request,pk):
-    post = Post.objects.get(pk=pk)
+    # success_url = reverse_lazy('blog:post_detail')         포스트 성공시 리다이렉트 아레 get_success_url로 대체가능
 
-# 자기가 작성한 유저가 아니라 다른 유저가 로그인하면 수정 , 삭제 안되게 막는 2줄 코드 -> 포스트 디테일로 돌아오게 만듬
-    if post.author != request.user:
-        return redirect('blog:post_detail', pk)
+    def form_valid(self,form):
+        post = form.save(commit=False)
+        post.author = self.request.user
+        post.save()
+        return super(PostCreateView,self).form_valid(form)
 
-    if request.method == 'POST':
-        form = PostForm(request.POST,request.FILES, instance=post)
-        if form.is_valid():
-            post = form.save()
-            return redirect('blog:post_detail', post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_forms.html',{
-        'form':form,
-    })
+    def get_success_url(self):
+        # redirect('blog:post_detail', self.object.pk)   #HttpResponseRedirect
+        # reverse('blog:post_detail', args=[self.object.pk], kwargs={})    #string
 
-@login_required
-def post_delete(request, pk):
-    post =Post.objects.get(pk=pk)
 
-    if post.author != request.user:
-        return redirect('blog:post_detail', pk)
+        return reverse('blog:post_detail', args=[self.object.pk])
 
-    if request.method == 'POST':
-        post.delete()
-        return redirect('blog:index')
-    return render(request, 'blog/post_delete_confirm.html',{
-        'post':post,
-    })
+post_create = login_required(PostCreateView.as_view())
+
+#
+# @login_required
+# def post_update(request,pk):
+#     post = Post.objects.get(pk=pk)
+#
+# # 자기가 작성한 유저가 아니라 다른 유저가 로그인하면 수정 , 삭제 안되게 막는 2줄 코드 -> 포스트 디테일로 돌아오게 만듬
+#     if post.author != request.user:
+#         return redirect('blog:post_detail', pk)
+#
+#     if request.method == 'POST':
+#         form = PostForm(request.POST,request.FILES, instance=post)
+#         if form.is_valid():
+#             post = form.save()
+#             return redirect('blog:post_detail', post.pk)
+#     else:
+#         form = PostForm(instance=post)
+#     return render(request, 'blog/post_forms.html',{
+#         'form':form,
+#     })
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_forms.html'
+
+
+    def get_success_url(self):
+        return reverse('blog:post_detail', args=[self.object.pk])
+
+post_update = PostUpdateView.as_view()
+
+# @login_required
+# def post_delete(request, pk):
+#     post =Post.objects.get(pk=pk)
+#
+#     if post.author != request.user:
+#         return redirect('blog:post_detail', pk)
+#
+#     if request.method == 'POST':
+#         post.delete()
+#         return redirect('blog:index')
+#     return render(request, 'blog/post_delete_confirm.html',{
+#         'post':post,
+#     })
+
+# post_delete = login_required(DeleteView.as_view(model=Post, success_url=reverse_lazy('blog:index'), template_name='blog/post_delete_confirm.html'))
+# 위 한줄 로직은 유저가 같을때 로직 구현 안돼있음
+
+# class PostDeleteView(DeleteView):
+#     model = Post
+#     success_url = reverse_lazy('blog:index')
+#     template_name='blog/post_delete_confirm.html'
+#     def dispatch(self, request, *args, **kwargs):
+#         if self.get_object().author != request.user:
+#             return redirect('blog:post_detail', self.kwargs['pk'])
+#         return super(PostDeleteView, self).dispatch(request, *args, **kwargs)
+#
+# post_delete = login_required(PostDeleteView.as_view())
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:index')
+    template_name='blog/post_delete_confirm.html'
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().author != request.user:
+            return redirect('blog:post_detail', self.kwargs['pk'])
+        return super(PostDeleteView, self).dispatch(request, *args, **kwargs)
+
+post_delete = PostDeleteView.as_view()
+#  장고가 1.9 일때 클래스에 로그인 믹스인 기능 할수있음 , 위에는 로그인 안돼있어도 바로 디테일뷰로 보내서 로그인페이지로 안감 로직수정이 필요
+
 
 @login_required
 def comment_new(request,pk):
